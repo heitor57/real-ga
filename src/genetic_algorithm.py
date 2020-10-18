4fa0d44b4bbd8e6dea604a623368cfa496389995
heitor57 <heitorwerneck@hotmail.com>
2020-10-18 17:43:13 -0300

    removed images, locals and parameters fix for class, config policies
    print config
    general config

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -19,25 +19,18 @@ import traceback
 import warnings
 import sys
 
-def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
-
-    log = file if hasattr(file,'write') else sys.stderr
-    traceback.print_stack(file=log)
-    log.write(warnings.formatwarning(message, category, filename, lineno, line))
-
-warnings.showwarning = warn_with_traceback
-
 parser = argparse.ArgumentParser()
-
-parameters = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+config = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+parameters = config['parameters']
 
 for k, v in parameters.items():
     parser.add_argument('--'+k,default=v['default'],type=type(v['default']))
 
 args = parser.parse_args()
 for k,v in vars(args).items():
-    exec(f"{k} = {v}")
-    exec(f"parameters['{k}']['value'] = {v}")
+    locals()[k] = v
+    parameters[k]['value'] = v
+    
 
 num_cross = int((cross_rate * num_pop)/2)
 num_no_cross = num_pop-2*num_cross
@@ -45,8 +38,8 @@ num_no_cross = num_pop-2*num_cross
 # information to catch from algorithm
 
 objective = Objective()
-cross_policy = BLXa(min_value,max_value)
-mutation_policy = UniformRandomSolutionSpace(mutation_rate,min_value,max_value)
+cross_policy = eval(parameters['cross_policy']['value'])(min_value,max_value)
+mutation_policy = eval(parameters['mutation_policy']['value'])(mutation_rate,min_value,max_value)
 
 population = []
 for i in range(num_pop):
@@ -66,7 +59,7 @@ for i in range(2,num_generations+1):
     new_population = []
     # Cross
     choices = list(range(len(population)))
-    selection_policy=Roulette(population)
+    selection_policy=eval(parameters['selection_policy']['value'])(population)
     for j in range(num_cross):
         ind1=selection_policy.select()
         ind2=selection_policy.select()
@@ -94,42 +87,44 @@ for i in range(2,num_generations+1):
     df.loc[i] = [','.join(map(lambda x: f'{x:.5f}',best_ind.genome)), f'{best_ind.ofv:.4}',np.mean([ind.ofv for ind in population])]
 df = df.reset_index()
 
-# PRINT TABLE BLOCK
-parameters_print = {key: val
-                    for key, val
-                    in zip([i['pretty_name'] for i in parameters.values()],
-                           [i['value'] for i in parameters.values()])}
-column_names_size = []
-max_size = max(map(len,parameters_print.keys()))
-max_size_val = max(map(len,map(str,parameters_print.values())))
+if config['general']['print_table']:
+    # PRINT TABLE BLOCK
+    parameters_print = {key: val
+                        for key, val
+                        in zip([i['pretty_name'] for i in parameters.values()],
+                            [i['value'] for i in parameters.values()])}
+    column_names_size = []
+    max_size = max(map(len,parameters_print.keys()))
+    max_size_val = max(map(len,map(str,parameters_print.values())))
 
-print(Colors.DARK,end='')
+    print(Colors.DARK,end='')
 
-for key, value in parameters_print.items():
-    print(Colors.Backgrounds.MAGENTA+key+' '*(max_size-len(key)),
-          Colors.Backgrounds.BLUE+str(value)+' '*(max_size_val-len(str(value)))+Colors.RESET_BACKGROUND)
-SPACED_STR = '   '
-for i, col in enumerate(columns):
-    col =SPACED_STR + col + SPACED_STR
-    print(COLORS[i],end='')
-    if i != 0:
-        print(col,end='')
-    else:
-        print(col,end='')
-    print(Colors.RESET_BACKGROUND,end='')
-    column_names_size.append(len(col))
-print()
-for index, row in df.iterrows():
-    print(Colors.Backgrounds.WHITE,end='')
-    for j, item in enumerate(row.T.to_list()):
-        item = str(item)
-        print(item,end='')
-        size = len(item)
-        print(" "*(len(columns[j]+SPACED_STR*2)-size),end='')
-        
-    print(Colors.RESET_BACKGROUND)
+    for key, value in parameters_print.items():
+        print(Colors.Backgrounds.MAGENTA+key+' '*(max_size-len(key)),
+            Colors.Backgrounds.BLUE+str(value)+' '*(max_size_val-len(str(value)))+Colors.RESET_BACKGROUND)
+    SPACED_STR = '   '
+    for i, col in enumerate(columns):
+        col =SPACED_STR + col + SPACED_STR
+        print(COLORS[i],end='')
+        if i != 0:
+            print(col,end='')
+        else:
+            print(col,end='')
+        print(Colors.RESET_BACKGROUND,end='')
+        column_names_size.append(len(col))
+    print()
+    for index, row in df.iterrows():
+        print(Colors.Backgrounds.WHITE,end='')
+        for j, item in enumerate(row.T.to_list()):
+            item = str(item)
+            print(item,end='')
+            size = len(item)
+            print(" "*(len(columns[j]+SPACED_STR*2)-size),end='')
 
-print(Colors.RESET,end='')
+        print(Colors.RESET_BACKGROUND)
+
+    print(Colors.RESET,end='')
+
 fout = open(DIRS['DATA_DIR']+get_parameters_name({k: v['value'] for k,v in parameters.items()})+'.json','w')
 fout.write(df.to_json(orient='records',lines=False))
 fout.close()
