51ef81bb21df16ebd4cf80d706817feee1692e1e
heitor57 <heitorwerneck@hotmail.com>
2020-10-19 19:14:39 -0300

    OneGene mutation policy

--- src/lib/mutation_policy.py
+++ src/lib/mutation_policy.py
@@ -15,3 +15,16 @@ class UniformRandomSolutionSpace(MutationPolicy):
                       i
                       for i in ind.genome]
         ind.genome = new_genome
+
+
+class OneGene(MutationPolicy):
+    def __init__(self,probability,min_value,max_value):
+        self.probability = probability
+        self.min_value = min_value
+        self.max_value = max_value
+    
+    def mutate(self,ind):
+        if self.probability > random.random():
+            i = random.randint(0,len(ind.genome)-1)
+            ind.genome[i] = random.uniform(self.min_value,self.max_value)
+
